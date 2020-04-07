9a7b984311969b61fce10934c560eb305f199aca
CarlosHWolox <carlos.hernandez@wolox.cl>
2020-04-07 11:46:44 -0400

    Fix albums

--- app/services/albums.js
+++ app/services/albums.js
@@ -2,7 +2,7 @@ const { api } = require('./config');
 
 exports.getAlbums = async () => {
   try {
-    const { data } = await api.get('/photos');
+    const { data } = await api.get('/albums');
     return data;
   } catch (e) {
     return { error: e };
