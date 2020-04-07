b03c2ff74c0c569fe41a3871ccb913eac5a6814a
CarlosHWolox <carlos.hernandez@wolox.cl>
2020-04-07 11:56:13 -0400

    Added album service called by id

--- app/services/albums.js
+++ app/services/albums.js
@@ -2,7 +2,16 @@ const { api } = require('./config');
 
 exports.getAlbums = async () => {
   try {
-    const { data } = await api.get('/photos');
+    const { data } = await api.get('/albums');
+    return data;
+  } catch (e) {
+    return { error: e };
+  }
+};
+
+exports.getAlbumsById = async id => {
+  try {
+    const { data } = await api.get(`/albums/${id}/photos`);
     return data;
   } catch (e) {
     return { error: e };
