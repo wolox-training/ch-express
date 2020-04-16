a3d4990c5656302942587a3be6345642de19e847
CarlosHWolox <carlos.hernandez@wolox.cl>
2020-04-16 10:40:31 -0400

    Change constants file ubication

--- app/services/config.js
+++ app/services/config.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const { BASE_URL_TIMEOUT } = require('./constants');
+const { BASE_URL_TIMEOUT } = require('../constants');
 
 exports.api = axios.create({
   baseURL: process.env.BASE_URL,
